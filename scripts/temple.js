cd5fe622ceb97747e30ffda1bc5e278a2539e727
Lincon Carmo <linconeduardodocarmo2@gmail.com>
2026-05-19 13:40:35 -0300

    change

---
+++ scripts/temple.js
@@ -0,0 +1,20 @@
+const menuButton = document.querySelector("#menu-button");
+const navMenu = document.querySelector("#nav-menu");
+
+menuButton.addEventListener("click", () => {
+
+  navMenu.classList.toggle("open");
+
+  if (navMenu.classList.contains("open")) {
+    menuButton.textContent = "X";
+  } else {
+    menuButton.textContent = "☰";
+  }
+
+});
+
+document.querySelector("#currentyear").textContent =
+  new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+  `Last Modified: ${document.lastModified}`;
