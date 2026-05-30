0c06f42fa90861d2937fcb71931b7057ba55a5b2
Lincon Carmo <linconeduardodocarmo2@gmail.com>
2026-05-30 12:38:36 -0300

    new project

---
+++ week03/scripts/place.js
@@ -0,0 +1,26 @@
+document.getElementById("currentyear").textContent =
+    new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
+
+const temperature = 8;
+const windSpeed = 10;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
+    ).toFixed(1);
+}
+
+const windChill = document.getElementById("windChill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
